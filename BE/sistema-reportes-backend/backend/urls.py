0a8cfb681c0c0e5cdf5a4980b1fc3dd065a0f5c4
JeaustinCfwd <jeaustincalderonulloafwd@gmail.com>
2025-11-14 13:51:16 -0600

    actualizaciones del frontend y backend(db json eliminado)

--- BE/sistema-reportes-backend/backend/urls.py
+++ BE/sistema-reportes-backend/backend/urls.py
@@ -3,8 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Importamos las vistas necesarias para JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 1. Rutas de Autenticación JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # 2. Rutas de la API de tu aplicación 'api'
     path('api/', include('api.urls')),
 ]
 
